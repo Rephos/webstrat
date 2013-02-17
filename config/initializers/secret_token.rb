f45a226ba5f30d6c471af7006b865b513dc14d97
Christopher Martin <chris.geass@gmail.com>
2013-02-17 17:54:02 +0100

    First commit
    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webstrat::Application.config.secret_token = 'b0882559476527a2429f38b01150ee97f7efa9712278ca7fb10a5ecb334f868ec0a3a292954bf7b07c23055f7b43c4278cc0679afda935b6509d0deca86c5e2c'
