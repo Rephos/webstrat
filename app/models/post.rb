e87cd4ce64c7652559187d7c91aaa99649a7ebda
Christopher Martin <chris.geass@gmail.com>
2013-02-20 14:52:50 +0100

    heroku - formation du blog

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   attr_accessible  :title, :content
 
   validates :title, presence: true, length: {in: 6..30}
-  validates :content, presence: true
+  validates :content, presence: true, length: {in: 5..500}
 
 end
